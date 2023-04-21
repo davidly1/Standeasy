548cf41d4e1559727b7dbf71a505365cfe5a079a
davidly <david26ly@gmail.com>
2023-04-21 16:19:34 -0400

    add barebones application

---
+++ src/components/Reminder.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Reminder() {
+  return (
+    <View>
+      <Text>Reminder</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/screens/Login.js
@@ -0,0 +1,14 @@
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <KeyboardAvoidingView>
+      <Text>Login</Text>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
