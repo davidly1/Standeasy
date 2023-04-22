d60ab131e5b00047dc83e241bb05e7291822ff73
davidly <david26ly@gmail.com>
2023-04-22 18:31:23 -0400

    Revert "setup firebase authentication"
    This reverts commit e9b2ebc89810c5172c4ed72223206c5a46fe78a3.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,94 +1,14 @@
-import { useNavigation } from '@react-navigation/native'
-import {
-  getAuth,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from 'firebase/auth'
-import React, { useState, useEffect } from 'react'
-import {
-  KeyboardAvoidingView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View
-} from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const auth = getAuth()
-
-  const navigation = useNavigation()
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) navigation.replace('Home')
-    })
-
-    return unsubscribe
-  }, [])
-
-  const handleSignUp = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredentials) => {
-        // Signed in
-        const user = userCredentials.user
-        console.log(`Registered with ${user.email}`)
-      })
-      .catch((err) => console.error(err.message))
-  }
-
-  const handleSignIn = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user
-        console.log(`Logged in with ${user.email}`)
-      })
-      .catch((err) => console.error(err.message))
-  }
-
   return (
-    <KeyboardAvoidingView behavior='padding'>
-      <View className='h-screen flex flex-col justify-center items-center'>
-        <Text className='text-center font-medium pb-4 text-lg text-gray-800'>
-          Login
-        </Text>
-        <TextInput
-          placeholder='Enter your email'
-          className='w-4/5 bg-white rounded-xl px-4 mb-2 py-1.5'
-          value={email}
-          onChangeText={(email) => setEmail(email)}
-          inputMode='email'
-        />
-        <TextInput
-          placeholder='Enter your password'
-          secureTextEntry
-          className='bg-white w-4/5 rounded-xl px-4 py-1.5'
-          value={password}
-          onChangeText={(password) => setPassword(password)}
-        />
-        <View className='flex flex-row gap-2 mt-4'>
-          <TouchableOpacity
-            className='bg-white rounded-lg p-2 mt-4 w-20'
-            onPress={() => {
-              handleSignIn
-            }}
-          >
-            <Text className='text-center'>Login</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            className='bg-white rounded-lg p-2 mt-4 w-20'
-            onPress={handleSignUp}
-          >
-            <Text className='text-center'>Register</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <KeyboardAvoidingView>
+      <Text>Login</Text>
     </KeyboardAvoidingView>
   )
 }
 
 export default Login
+
+const styles = StyleSheet.create({})
